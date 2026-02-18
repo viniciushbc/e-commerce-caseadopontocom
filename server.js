37dee345351094d57265c28d46428a567b3a0900
viniciushbc <vinehbc@gmail.com>
2026-02-18 13:16:48 -0300

    feat: stripe checkout routes and basic setup

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: 'embedded',
+    line_items: [
+      {
+        //  Price ID of the product
+        price: 'price_1Sx9ygAjHi7WKjcWSS2W4PK9',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    return_url: `${process.env.DOMAIN_URL}:${process.env.PORT}/return.html?session_id={CHECKOUT_SESSION_ID}`,
+  });
+
+  res.send({clientSecret: session.client_secret});
+});
+
+app.get('/session-status', async (req, res) => {
+  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+
+  res.send({
+    status: session.status,
+    customer_email: session.customer_details.email
+  });
+});
+
+app.listen(process.env.PORT, () => console.log('Running on port 3000'));

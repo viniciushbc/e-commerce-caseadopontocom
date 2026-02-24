24120bb635815e9c2304b64899f831441da504d9
viniciushbc <vinehbc@gmail.com>
2026-02-24 03:56:58 -0300

    feat(admin): add admin products route

---
+++ backend/src/routes/admin.routes.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import { adminAuth } from '../middlewares/adminAuth.js';
+import { upload } from '../services/multer.js';
+
+export function adminRoutes({ adminCreateProductService}) {
+    const router = express.Router();
+
+
+    router.post(
+        "/products",
+        adminAuth,
+        upload.fields([
+            {name: "images", maxCount: 10},
+            {name: "file", maxCount: 1}
+        ]),
+        async (req, res) => {
+            try {
+                const {name, description, price, currency} = req.body
+                const images = req.files?.images || [];
+                const pdf = req.files?.file?.[0];
+
+                const result = await adminCreateProductService.create({
+                    name,
+                    description,
+                    price,
+                    currency: currency || "brl",
+                    images,
+                    pdf
+                })
+
+                return res.status((201).json(result))
+            } catch (e) {
+                const status = e.status || 500
+
+                const msg =
+                    e.message === "NAME_REQUIRED" ? "Nome é obrigatório" :
+                    e.message === "PRICE_INVALID" ? "Preço inválido" :
+                    e.message === "PDF_REQUIRED" ? "PDF é obrigatório" :
+                    e.message === "IMAGES_REQUIRED" ? "Envie ao menos 1 imagem" :
+                    "Erro ao cadastrar produto";
+
+                    console.error("admin/products error:", e.message, e.cause || "");
+                    return res.status(status).json({ error: msg });
+            }
+        }
+    )
+
+    return router;
+}
